0e4a1f94d16892de1a61a208778fde51394dafd0
Matías Barisone <matibarisone3@gmail.com>
2024-10-09 16:43:34 -0300

    BCRYPT

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-process.loadEnvFile()
+require('dotenv').config(); // Carga el archivo .env si lo necesitas
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
@@ -10,12 +10,23 @@ app.get('/', (req, res) => {
   res.json({ message: 'Bienvenid@s a la API de Bcryptjs.' })
 })
 
+//GENERACION DE ENCRIPTADO
 app.post('/encriptar', async (req, res) => {
-
+  async function encriptarPWD() {
+    const saltRounds = parseInt(process.env.SALTROUNDS); // Read SALTROUNDS from .env
+    try {
+      res.status(201).json({ message: 'Generación de hash exitosa.', saltRounds });
+    } catch (error) {
+      res.status(400).json('Debe enviar una clave válida para encriptar');
+    }  
+}
 })
 
 app.post('/comparar', (req, res) => {
-  const hashGenerado = 'agrega-aqui-el-hash-obtenido-con-el-otro-endpoint'
-})
+  try{
+    const {password, hash}
+  }
+}
+
 
 app.listen(port, () => console.log(`Servidor escuchando en el puerto ${port}`))
